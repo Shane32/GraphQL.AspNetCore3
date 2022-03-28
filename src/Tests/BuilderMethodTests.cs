94797329b521cb97f94a6ae8f2ede7c270dede11
Shane32 <shane@acdmail.com>
2022-03-28 17:23:25 -0400

    Add user context builder tests

--- src/Tests/BuilderMethodTests.cs
+++ src/Tests/BuilderMethodTests.cs
@@ -23,8 +23,27 @@ public class BuilderMethodTests
                 .AddAutoSchema<Chat.Schema.Query>(s => s
                     .WithMutation<Chat.Schema.Mutation>()
                     .WithSubscription<Chat.Schema.Subscription>())
+                .AddSchema<Schema2>()
                 .AddSystemTextJson());
         });
+    }
+
+    private class Schema2 : Schema
+    {
+        public Schema2(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            Query = serviceProvider.GetRequiredService<AutoRegisteringObjectGraphType<Query2>>();
+        }
+    }
+
+    public class Query2
+    {
+        public static string? UserInfo([FromUserContext] MyUserContext myContext) => myContext.UserInfo;
+    }
+
+    public class MyUserContext : Dictionary<string, object?>
+    {
+        public string? UserInfo { get; set; }
     }
 
     [Fact]
@@ -130,6 +149,90 @@ public class BuilderMethodTests
             });
         });
         await VerifyAsync();
+    }
+
+    [Fact]
+    public async Task WebSocketHandler_Configure()
+    {
+        _hostBuilder.ConfigureServices(services => {
+            services.AddGraphQL(b => b.AddWebSocketHandler(c => { }));
+        });
+        _hostBuilder.Configure(app => {
+            app.UseWebSockets();
+            app.UseGraphQL();
+        });
+        await VerifyAsync();
+    }
+
+    [Fact]
+    public async Task WebSocketHandler_Configure2()
+    {
+        _hostBuilder.ConfigureServices(services => {
+            services.AddGraphQL(b => b.AddWebSocketHandler((c, p) => { }));
+        });
+        _hostBuilder.Configure(app => {
+            app.UseWebSockets();
+            app.UseGraphQL();
+        });
+        await VerifyAsync();
+    }
+
+    [Fact]
+    public async Task WebSocketHandler_Configure3()
+    {
+        _hostBuilder.ConfigureServices(services => {
+            services.AddSingleton<WebSocketHandlerOptions>();
+            services.AddGraphQL(b => b.AddWebSocketHandler<WebSocketHandler<ISchema>>());
+        });
+        _hostBuilder.Configure(app => {
+            app.UseWebSockets();
+            app.UseGraphQL();
+        });
+        await VerifyAsync();
+    }
+
+    [Fact]
+    public async Task UserContextBuilder_Configure1()
+    {
+        _hostBuilder.ConfigureServices(services => {
+            services.AddGraphQL(b => b.AddUserContextBuilder<MyUserContextBuilder>());
+        });
+        await VerifyUserContextAsync("fromBuilder");
+    }
+
+    [Fact]
+    public async Task UserContextBuilder_Configure2()
+    {
+        _hostBuilder.ConfigureServices(services => {
+            services.AddGraphQL(b => b.AddUserContextBuilder(context => Task.FromResult(new MyUserContext { UserInfo = "test2" })));
+        });
+        await VerifyUserContextAsync("test2");
+    }
+
+    [Fact]
+    public async Task UserContextBuilder_Configure3()
+    {
+        _hostBuilder.ConfigureServices(services => {
+            services.AddGraphQL(b => b.AddUserContextBuilder(context => new MyUserContext { UserInfo = "test3" }));
+        });
+        await VerifyUserContextAsync("test3");
+    }
+
+    private class MyUserContextBuilder : IUserContextBuilder
+    {
+        public ValueTask<IDictionary<string, object?>> BuildUserContextAsync(HttpContext context)
+            => new ValueTask<IDictionary<string, object?>>(new MyUserContext { UserInfo = "fromBuilder" });
+    }
+
+    private async Task VerifyUserContextAsync(string value)
+    {
+        _hostBuilder.Configure(app => {
+            app.UseWebSockets();
+            app.UseGraphQL<Schema2>();
+        });
+        using var server = new TestServer(_hostBuilder);
+        var str = await server.ExecuteGet("/graphql?query={userInfo}");
+        str.ShouldBe(@"{""data"":{""userInfo"":""" + value + @"""}}");
     }
 
     private async Task VerifyAsync()
