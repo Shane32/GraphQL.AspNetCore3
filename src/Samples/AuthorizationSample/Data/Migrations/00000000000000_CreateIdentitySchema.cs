c6fca4552d4aba8b1f1575c25009881c543f8fd1
Shane32 <shane@acdmail.com>
2022-04-11 12:05:54 -0400

    Formatting

--- src/Samples/AuthorizationSample/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ src/Samples/AuthorizationSample/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using System;
 
 namespace AuthorizationSample.Data.Migrations;
 
@@ -10,22 +9,19 @@ public partial class CreateIdentitySchema : Migration
     {
         migrationBuilder.CreateTable(
             name: "AspNetRoles",
-            columns: table => new
-            {
+            columns: table => new {
                 Id = table.Column<string>(nullable: false),
                 Name = table.Column<string>(maxLength: 256, nullable: true),
                 NormalizedName = table.Column<string>(maxLength: 256, nullable: true),
                 ConcurrencyStamp = table.Column<string>(nullable: true)
             },
-            constraints: table =>
-            {
+            constraints: table => {
                 table.PrimaryKey("PK_AspNetRoles", x => x.Id);
             });
 
         migrationBuilder.CreateTable(
             name: "AspNetUsers",
-            columns: table => new
-            {
+            columns: table => new {
                 Id = table.Column<string>(nullable: false),
                 UserName = table.Column<string>(maxLength: 256, nullable: true),
                 NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
@@ -42,23 +38,20 @@ public partial class CreateIdentitySchema : Migration
                 LockoutEnabled = table.Column<bool>(nullable: false),
                 AccessFailedCount = table.Column<int>(nullable: false)
             },
-            constraints: table =>
-            {
+            constraints: table => {
                 table.PrimaryKey("PK_AspNetUsers", x => x.Id);
             });
 
         migrationBuilder.CreateTable(
             name: "AspNetRoleClaims",
-            columns: table => new
-            {
+            columns: table => new {
                 Id = table.Column<int>(nullable: false)
                     .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
                 RoleId = table.Column<string>(nullable: false),
                 ClaimType = table.Column<string>(nullable: true),
                 ClaimValue = table.Column<string>(nullable: true)
             },
-            constraints: table =>
-            {
+            constraints: table => {
                 table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
                 table.ForeignKey(
                     name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
@@ -70,16 +63,14 @@ public partial class CreateIdentitySchema : Migration
 
         migrationBuilder.CreateTable(
             name: "AspNetUserClaims",
-            columns: table => new
-            {
+            columns: table => new {
                 Id = table.Column<int>(nullable: false)
                     .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
                 UserId = table.Column<string>(nullable: false),
                 ClaimType = table.Column<string>(nullable: true),
                 ClaimValue = table.Column<string>(nullable: true)
             },
-            constraints: table =>
-            {
+            constraints: table => {
                 table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
                 table.ForeignKey(
                     name: "FK_AspNetUserClaims_AspNetUsers_UserId",
@@ -91,15 +82,13 @@ public partial class CreateIdentitySchema : Migration
 
         migrationBuilder.CreateTable(
             name: "AspNetUserLogins",
-            columns: table => new
-            {
+            columns: table => new {
                 LoginProvider = table.Column<string>(maxLength: 128, nullable: false),
                 ProviderKey = table.Column<string>(maxLength: 128, nullable: false),
                 ProviderDisplayName = table.Column<string>(nullable: true),
                 UserId = table.Column<string>(nullable: false)
             },
-            constraints: table =>
-            {
+            constraints: table => {
                 table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
                 table.ForeignKey(
                     name: "FK_AspNetUserLogins_AspNetUsers_UserId",
@@ -111,13 +100,11 @@ public partial class CreateIdentitySchema : Migration
 
         migrationBuilder.CreateTable(
             name: "AspNetUserRoles",
-            columns: table => new
-            {
+            columns: table => new {
                 UserId = table.Column<string>(nullable: false),
                 RoleId = table.Column<string>(nullable: false)
             },
-            constraints: table =>
-            {
+            constraints: table => {
                 table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
                 table.ForeignKey(
                     name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
@@ -135,15 +122,13 @@ public partial class CreateIdentitySchema : Migration
 
         migrationBuilder.CreateTable(
             name: "AspNetUserTokens",
-            columns: table => new
-            {
+            columns: table => new {
                 UserId = table.Column<string>(nullable: false),
                 LoginProvider = table.Column<string>(maxLength: 128, nullable: false),
                 Name = table.Column<string>(maxLength: 128, nullable: false),
                 Value = table.Column<string>(nullable: true)
             },
-            constraints: table =>
-            {
+            constraints: table => {
                 table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
                 table.ForeignKey(
                     name: "FK_AspNetUserTokens_AspNetUsers_UserId",
