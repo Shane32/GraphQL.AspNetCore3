7be4188b3874303162968f429b4e45fd0c30bbf6
Shane Krueger <Shane@acdmail.com>
2022-05-22 02:13:12 -0400

    Update

--- src/GraphQL.AspNetCore3/AuthorizationVisitorBase.cs
+++ src/GraphQL.AspNetCore3/AuthorizationVisitorBase.cs
@@ -200,6 +200,7 @@ public abstract partial class AuthorizationVisitorBase : INodeVisitor
     {
         // first see if any other fragments are waiting on this fragment
         if (_fragments != null) {
+        Retry:
             foreach (var fragment in _fragments) {
                 var ti2 = fragment.Value;
                 if (ti2.WaitingOnFragments != null && ti2.WaitingOnFragments.Remove(fragmentName)) {
@@ -207,6 +208,7 @@ public abstract partial class AuthorizationVisitorBase : INodeVisitor
                     ti2.AnyAnonymous |= ti.AnyAnonymous;
                     _fragments[fragment.Key] = ti2;
                     RecursiveResolve(fragment.Key, ti2, context);
+                    goto Retry; // modifying a collection at runtime is not supported
                 }
             }
         }
