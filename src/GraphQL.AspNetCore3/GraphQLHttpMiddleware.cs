09f609e8e121a3264ba2851c505cfcd5e9d96549
Shane32 <shane@acdmail.com>
2022-06-05 13:05:09 -0400

    Update formatting

--- src/GraphQL.AspNetCore3/GraphQLHttpMiddleware.cs
+++ src/GraphQL.AspNetCore3/GraphQLHttpMiddleware.cs
@@ -258,13 +258,10 @@ public abstract class GraphQLHttpMiddleware
                 try {
 #if NET5_0_OR_GREATER
                     // Wrap content stream into a transcoding stream that buffers the data transcoded from the sourceEncoding to utf-8.
-                    if (sourceEncoding != null && sourceEncoding != System.Text.Encoding.UTF8)
-                    {
+                    if (sourceEncoding != null && sourceEncoding != System.Text.Encoding.UTF8) {
                         using var tempStream = System.Text.Encoding.CreateTranscodingStream(httpRequest.Body, innerStreamEncoding: sourceEncoding, outerStreamEncoding: System.Text.Encoding.UTF8, leaveOpen: true);
                         deserializationResult = await _serializer.ReadAsync<IList<GraphQLRequest?>>(tempStream, context.RequestAborted);
-                    }
-                    else
-                    {
+                    } else {
                         deserializationResult = await _serializer.ReadAsync<IList<GraphQLRequest?>>(httpRequest.Body, context.RequestAborted);
                     }
 #else
