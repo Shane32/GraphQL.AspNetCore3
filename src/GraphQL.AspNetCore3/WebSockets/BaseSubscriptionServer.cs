71d931243a7ebfd5c63957a4295eef61e4a454dd
Shane32 <shane@acdmail.com>
2022-06-10 19:04:16 -0400

    Update

--- src/GraphQL.AspNetCore3/WebSockets/BaseSubscriptionServer.cs
+++ src/GraphQL.AspNetCore3/WebSockets/BaseSubscriptionServer.cs
@@ -8,7 +8,7 @@ namespace GraphQL.AspNetCore3.WebSockets;
 /// Manages a WebSocket message stream.
 /// </summary>
 /// <remarks>
-/// All methods except <see cref="OnMessageReceivedAsync">OnMessageReceivedAsync</see> should be thread-safe.
+/// All methods except <see cref="OnMessageReceivedAsync(OperationMessage)">OnMessageReceivedAsync</see> should be thread-safe.
 /// <see cref="OnMessageReceivedAsync(OperationMessage)">OnMessageReceivedAsync</see> will receive an ordered
 /// sequence of messages from the client, dispatched by <see cref="IWebSocketConnection"/>.
 /// </remarks>
