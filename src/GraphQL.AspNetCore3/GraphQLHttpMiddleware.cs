00e60784c7659794c69934af6f15ba72fc48bf26
Shane32 <shane@acdmail.com>
2024-12-01 14:39:08 -0500

    Update

--- src/GraphQL.AspNetCore3/GraphQLHttpMiddleware.cs
+++ src/GraphQL.AspNetCore3/GraphQLHttpMiddleware.cs
@@ -5,7 +5,6 @@ using System.Globalization;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace GraphQL.AspNetCore3;
 
